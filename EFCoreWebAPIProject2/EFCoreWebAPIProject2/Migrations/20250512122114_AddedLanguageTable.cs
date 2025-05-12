ca1d0cf190d1a6413ba6130d2359efdea08d8871
ShubhamDev021 <shubhat021@gmail.com>
2025-05-12 17:52:49 +0530

    Added language table migration - Add-Migration AddedLanguageTable

---
+++ EFCoreWebAPIProject2/EFCoreWebAPIProject2/Migrations/20250512122114_AddedLanguageTable.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreWebAPIProject2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLanguageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageId",
+                table: "Books",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_LanguageId",
+                table: "Books",
+                column: "LanguageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Languages_LanguageId",
+                table: "Books",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Languages_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageId",
+                table: "Books");
+        }
+    }
+}
